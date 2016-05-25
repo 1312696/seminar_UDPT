e8ea8ef780f167b46006045b4240848bd48435e4
tudinh29 <1312676@student.hcmus.edu.vn>
2016-05-25 13:13:55 +0700

    Mẫu canvas

---
+++ javascript/canvas.js
@@ -0,0 +1,13 @@
+
+
+
+function Ve2() {
+   var canvas = document.getElementsByTagName( 'canvas' )[ 1 ];
+    var ctx2 = canvas.getContext('2d');
+ 	ctx2.font = '40pt Calibri';
+    ctx2.fillStyle = 'Black';
+ 	ctx2.fillText("Your score", 50, 70);
+    // vẽ một đoạn text tại vị trí 150px 100px
+
+}
+Ve2();
