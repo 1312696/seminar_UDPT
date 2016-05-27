2fcd5d61930c440477ba31c4fb6298a683748b13
htantruc <1312653@student.hcmus.edu.vn>
2016-05-27 16:53:30 +0700

    các hàm để khung thứ 2 chạy

--- javascript/canvas2.js
+++ javascript/canvas2.js
@@ -2,14 +2,14 @@ var canvas3 = document.getElementsByTagName( 'canvas' )[ 3 ];
 var ctx3 = canvas3.getContext('2d');
 var canvas1 = document.getElementsByTagName( 'canvas' )[ 2 ];
 var ctx1 = canvas1.getContext( '2d' );
-var chieuNgang = 300, chieuDoc = 600;
-var canh = chieuNgang / 10;
-var danhSachMau = [
+var chieuNgang2 = 300, chieuDoc2 = 600;
+var canh2 = chieuNgang2 / 10;
+var danhSachMau2 = [
     'cyan', 'orange', 'blue', 'yellow', 'red', 'green', 'purple'
 ];
 function Ve1( x, y ) {
-    ctx.fillRect( canh * x, canh * y, canh - 1 , canh - 1 );
-    ctx.strokeRect( canh * x, canh * y, canh - 1 , canh - 1 );
+    ctx1.fillRect( canh2 * x, canh2 * y, canh2 - 1 , canh2 - 1 );
+    ctx1.strokeRect( canh2 * x, canh2 * y, canh2 - 1 , canh2 - 1 );
 }
 function Ve2() {
    //ctx2.clearRect( 0, 0, W, H );
@@ -17,13 +17,13 @@ function Ve2() {
     ctx3.fillStyle = 'Black';
  	ctx3.fillText("Your score", 10, 100);
 	ctx3.font = '23pt Calibri';
-    ctx3.fillText(DiemSo, 50, 150);
-    ctx1.clearRect( 0, 0, chieuNgang, chieuDoc );
+    ctx3.fillText(DiemSo2, 50, 150);
+    ctx1.clearRect( 0, 0, chieuNgang2, chieuDoc2 );
     ctx1.strokeStyle = 'black';
-    for ( var x = 0; x < cotBang; ++x ) {
-        for ( var y = 0; y < dongBang; ++y ) {
-            if ( bangGiaTri[ y ][ x ] ) {
-                ctx1.fillStyle = danhSachMau[ bangGiaTri[ y ][ x ] - 1 ];
+    for ( var x = 0; x < cotBang2; ++x ) {
+        for ( var y = 0; y < dongBang2; ++y ) {
+            if ( bangGiaTri2[ y ][ x ] ) {
+                ctx1.fillStyle = danhSachMau2[ bangGiaTri2[ y ][ x ] - 1 ];
                 Ve1( x, y );
             }
         }
@@ -32,9 +32,9 @@ function Ve2() {
     ctx1.strokeStyle = 'black';
     for ( var y = 0; y < 4; ++y ) {
         for ( var x = 0; x < 4; ++x ) {
-            if ( ViTriXoay[ y ][ x ] ) {
-                ctx1.fillStyle = danhSachMau[ ViTriXoay[ y ][ x ] - 1 ];
-                Ve1( viTriX + x, viTriY + y );
+            if ( ViTriXoay2[ y ][ x ] ) {
+                ctx1.fillStyle = danhSachMau2[ ViTriXoay2[ y ][ x ] - 1 ];
+                Ve1( viTriX2 + x, viTriY2 + y );
             }
         }
     }
--- javascript/game2.js
+++ javascript/game2.js
@@ -1,13 +1,13 @@
-var cotBang = 10;
-var dongBang = 20;
-var DiemSo;
-var bangGiaTri = [];
-var ViTriXoay; 
-var viTriX, viTriY; 
-var Tocdo = 300;
-var Thua;
-var interval;
-var danhSachHinh = [
+var cotBang2 = 10;
+var dongBang2 = 20;
+var DiemSo2;
+var bangGiaTri2 = [];
+var ViTriXoay2; 
+var viTriX2, viTriY2; 
+var Tocdo2 = 300;
+var Thua2;
+var interval2;
+var danhSachHinh2 = [
     [ 1, 1, 1, 1, 0, 0, 0, 0],
     [ 1, 1, 1, 0, 1 ],
     [ 1, 1, 1, 0, 0, 0, 1 ],
@@ -16,127 +16,144 @@ var danhSachHinh = [
     [ 0, 1, 1, 0, 1, 1 ],
     [ 0, 1, 0, 0, 1, 1, 1 ]
 ];
-function KhoiTao() {
-    DiemSo = 0;
-    for ( var x = 0; x < dongBang; x++ ) {
-        bangGiaTri[x] = [];
-        for ( var y = 0; y < cotBang; y++ ) {
-            bangGiaTri[x][y] = 0;
+function KhoiTao2() {
+    DiemSo2 = 0;
+    for ( var x = 0; x < dongBang2; x++ ) {
+        bangGiaTri2[x] = [];
+        for ( var y = 0; y < cotBang2; y++ ) {
+            bangGiaTri2[x][y] = 0;
         }
     }
 }
-function TaoHinhMoi() {
-    viTriX = 4;
-    viTriY = 0;
-    ViTriXoay = [];
-    var id = Math.floor( Math.random() * danhSachHinh.length );
-    var hinh = danhSachHinh[ id ];
+function TaoHinhMoi2() {
+    viTriX2 = 4;
+    viTriY2 = 0;
+    ViTriXoay2 = [];
+    var id2 = Math.floor( Math.random() * danhSachHinh2.length );
+    var hinh2 = danhSachHinh2[ id2 ];
     for ( var x = 0; x < 4; x++ )
     {
-        ViTriXoay[ x ] = [];
+        ViTriXoay2[ x ] = [];
         for ( var y = 0; y < 4; y++ ) 
         {
             var vt = 4 * x + y;
-            if ( typeof hinh[ vt ] != 'undefined' && hinh[ vt ] ) {
-                ViTriXoay[ x ][ y ] = id + 1;
+            if ( typeof hinh2[ vt ] != 'undefined' && hinh2[ vt ] ) {
+                ViTriXoay2[ x ][ y ] = id2 + 1;
             }
             else {
-                ViTriXoay[ x ][ y ] = 0;
+                ViTriXoay2[ x ][ y ] = 0;
             }
         }
     }
 }
 //Tăng tọa độ y cho ô gạch
-function vongLap(){
-  if ( kiemTra( 0, 1 ) ) {
-        viTriY = viTriY+1;
+function vongLap2(){
+  if ( kiemTra2( 0, 1 ) ) {
+        viTriY2 = viTriY2+1;
     }
     else {
-        luuGiaTri();
-        xoaHang();
-        if (Thua == 1) {
-            taoTroChoi();
+        luuGiaTri2();
+        xoaHang2();
+        if (Thua2 == 1) {
+            taoTroChoi2();
             return false;
         }
-        TaoHinhMoi();
+        TaoHinhMoi2();
     }
 }
 //luu gia tri vao bang khi o gạch xuống đáy
-function  luuGiaTri() {
+function  luuGiaTri2() {
     for ( var x = 0; x < 4; x++ ) {
         for ( var y = 0; y < 4; y++ ) {
-            if ( ViTriXoay[ x ][ y ] != 0 ) {
-                bangGiaTri[ x + viTriY ][ y + viTriX ] = ViTriXoay[ x ][ y ];
+            if ( ViTriXoay2[ x ][ y ] != 0 ) {
+                bangGiaTri2[ x + viTriY2 ][ y + viTriX2 ] = ViTriXoay2[ x ][ y ];
             }
         }
     }
 }
-function  xoay(ViTriXoay) {
-    var viTriMoi = [];
+function  xoay2(ViTriXoay2) {
+    var viTriMoi2 = [];
     for ( var y = 0; y < 4; ++y ) {
-        viTriMoi[ y ] = [];
+        viTriMoi2[ y ] = [];
         for ( var x = 0; x < 4; ++x ) {
-            viTriMoi[ y ][ x ] = ViTriXoay[ 3 - x ][ y ];
+            viTriMoi2[ y ][ x ] = ViTriXoay2[ 3 - x ][ y ];
         }
     }
 
-    return viTriMoi;
+    return viTriMoi2;
 }
-function  xoaHang() {
-    
+function  xoaHang2() {
+    for ( var y = dongBang2 - 1; y >= 0; --y ) {
+        var dayDong2 = true;
+        for ( var x = 0; x < cotBang2; ++x ) {
+            if ( bangGiaTri2[ y ][ x ] == 0 ) {
+                dayDong2 = false;
+                break;
+            }
+        }
+        if ( dayDong2 ) {
+           
+            for ( var yy = y; yy > 0; --yy ) {
+                for ( var x = 0; x < cotBang2; ++x ) {
+                    bangGiaTri2[ yy ][ x ] = bangGiaTri2[ yy - 1 ][ x ];
+                }
+            }
+            ++y;
+        }
+    }
 }
 
 
 //khi diem cang cao thi goi ham de tang toc do roi cua cac khoi
-function tangTocDo(){
-        Tocdo = 300 - (DiemSo / 300) * 10;
+function tangTocDo2(){
+        Tocdo2 = 300 - (DiemSo2 / 300) * 10;
 }
 
 //xu ly su kiem phim
-function bamPhim( phim ) {
+function bamPhim2( phim ) {
     switch ( phim ) {
         case 'trai2':
-            if ( kiemTra( -1,0, null ) ) {
-                --viTriX;
+            if ( kiemTra2( -1,0, null ) ) {
+                --viTriX2;
             }
             break;
         case 'phai2':
-            if ( kiemTra( 1,0,null ) ) {
-                ++viTriX;
+            if ( kiemTra2( 1,0,null ) ) {
+                ++viTriX2;
             }
             break;
         case 'xuong2':
-            if ( kiemTra( 0, 1, null ) ) {
-                ++viTriY;
+            if ( kiemTra2( 0, 1, null ) ) {
+                ++viTriY2;
             }
             break;
         case 'xoay2':
-            var khoixoay = xoay( ViTriXoay );
-            if ( kiemTra( 0, 0, khoixoay ) ) {
-                ViTriXoay = khoixoay;
+            var khoixoay2 = xoay2( ViTriXoay2 );
+            if ( kiemTra2( 0, 0, khoixoay2 ) ) {
+                ViTriXoay2 = khoixoay2;
             }
             break;
     }
 }
 
 //kiem tra vi tri cua khoi co kha thi hay khong
-function kiemTra(X, Y, viTriMoi){
-    X = viTriX + X;
-    Y = viTriY + Y;
-    viTriMoi = viTriMoi || ViTriXoay; // khi xoay khoi co bi qua ngoai man hinh khong
+function kiemTra2(X, Y, viTriMoi){
+    X = viTriX2 + X;
+    Y = viTriY2 + Y;
+    viTriMoi = viTriMoi || ViTriXoay2; // khi xoay khoi co bi qua ngoai man hinh khong
 
 
 
     for ( var y = 0; y < 4; ++y ) {
         for ( var x = 0; x < 4; ++x ) {
             if ( viTriMoi [ y ][ x ] ) {
-                if ( typeof bangGiaTri[ y + Y ] == 'undefined'   // kiem tra cac Dk cua khoi co kha thi hay khong, khong ra ngoai khung hinh
-                  || typeof bangGiaTri[ y + Y ][ x + X ] == 'undefined'
-                  || bangGiaTri[ y + Y ][ x + X ]
+                if ( typeof bangGiaTri2[ y + Y ] == 'undefined'   // kiem tra cac Dk cua khoi co kha thi hay khong, khong ra ngoai khung hinh
+                  || typeof bangGiaTri2[ y + Y ][ x + X ] == 'undefined'
+                  || bangGiaTri2[ y + Y ][ x + X ]
                   || x + X < 0
-                  || y + Y >= dongBang
-                  || x + X>= cotBang ) {
-                    if (Y == 1) Thua = true; // neu khoi dang o dong tren cung thi thua
+                  || y + Y >= dongBang2
+                  || x + X>= cotBang2 ) {
+                    if (Y == 1) Thua2 = true; // neu khoi dang o dong tren cung thi thua
                     return false;
                 }
             }
@@ -147,11 +164,11 @@ function kiemTra(X, Y, viTriMoi){
 
 
 //tao moi tro choi
-function taoTroChoi() {
-    clearInterval(interval);
-    KhoiTao();
-    TaoHinhMoi();
-    Thua = false;
-    interval = setInterval( vongLap, Tocdo)
+function taoTroChoi2() {
+    clearInterval(interval2);
+    KhoiTao2();
+    TaoHinhMoi2();
+    Thua2 = false;
+    interval2 = setInterval( vongLap2, Tocdo2)
 }
-taoTroChoi()
+taoTroChoi2()
