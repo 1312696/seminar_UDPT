45ddc3234530876ba2a62e9708062fc6b5026f07
1312696 <1312696@student.hcmus.edu.vn>
2016-05-27 15:23:54 +0700

    bắt sự kiện bấm phím

---
+++ javascript/sukienphim.js
@@ -0,0 +1,12 @@
+document.body.onkeydown = function( e ) {
+    var phim = {
+        37: 'trai',
+        39: 'phai',
+        40: 'xuong',
+        38: 'xoay'
+    };
+    if ( typeof phim[ e.keyCode ] != 'undefined' ) {
+        bamPhim( phim[ e.keyCode ] );
+        render();
+    }
+};
